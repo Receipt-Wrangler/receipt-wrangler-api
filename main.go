015e2a6d86a7c03948bce0bb7278ea345e144f45
Noah <noah231515@gmail.com>
2022-07-21 13:12:49 -0400

    remove commented code

--- main.go
+++ main.go
@@ -7,28 +7,4 @@ import (
 func main() {
 	db.Connect()
 	db.MakeMigrations()
-
-	// environment.SetEnv()
-	// router := mux.NewRouter()
-	// database.ConnectToDatabase()
-	// database.MakeMigrations()
-	// validator, err := crypto_utils.InitJwtValidator()
-
-	// if err == nil {
-	// 	initRoutes(router, validator)
-	// 	srv := &http.Server{
-	// 		Handler:      router,
-	// 		Addr:         "127.0.0.1:8081", // TODO: make configurable
-	// 		WriteTimeout: 15 * time.Second,
-	// 		ReadTimeout:  15 * time.Second,
-	// 	}
-	// 	log.Fatal(srv.ListenAndServe())
-	// } else {
-	// 	log.Fatal(err)
-	// }
-
 }
-
-// func initRoutes(router *mux.Router, validator *validator.Validator) {
-
-// }

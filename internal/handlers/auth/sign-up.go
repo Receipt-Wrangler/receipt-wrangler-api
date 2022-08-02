7d989dd845e9dcf9c1d142e6b186d2be6d5383c8
Noah <noah231515@gmail.com>
2022-08-01 22:03:05 -0400

    write to user table

--- internal/handlers/auth/sign-up.go
+++ internal/handlers/auth/sign-up.go
@@ -3,13 +3,28 @@ package signUp
 import (
 	"fmt"
 	"net/http"
+	db "receipt-wrangler/api/internal/database"
 	"receipt-wrangler/api/internal/models"
+	httpUtils "receipt-wrangler/api/internal/utils/http"
 )
 
 func SignUp(w http.ResponseWriter, r *http.Request) {
 	validateData()
+
+	db := db.GetDB()
+
 	userData := r.Context().Value("user").(models.User)
 	fmt.Println(userData)
+
+	// hash password
+
+	result := db.Create(&userData)
+
+	if result.Error != nil {
+		httpUtils.WriteErrorResponse(w, result.Error, 500)
+		return
+	}
+
 	w.Write([]byte("hello there"))
 }
 

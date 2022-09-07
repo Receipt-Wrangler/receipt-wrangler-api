b2d71fbb40ec42b3d0fd3e2f82923f800e324e50
Noah <noah231515@gmail.com>
2022-09-07 09:04:16 -0400

    use json correctly

--- internal/models/receipt.go
+++ internal/models/receipt.go
@@ -4,8 +4,8 @@ type Receipt struct {
 	BaseModel
 	Name          string `gorm:"not null" json:"name"`
 	ImgPath       string `json:"-"`
-	PaidByUserID  uint
-	PaidByUser    User `json:"-"`
+	PaidByUserID  uint   `json:"paidByUserId"`
+	PaidByUser    User   `json:"-"`
 	OwnedByUserID uint
 	OwnedByUser   User `json:"-"`
 }

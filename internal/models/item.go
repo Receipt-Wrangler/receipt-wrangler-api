bdc87b64e667bc1720c9de2e1185e5f977bf8837
Noah <noah231515@gmail.com>
2022-11-04 09:03:10 -0400

    update json props

--- internal/models/item.go
+++ internal/models/item.go
@@ -4,10 +4,10 @@ import "github.com/shopspring/decimal"
 
 type Item struct {
 	BaseModel
-	Name            string `gorm:"not null"`
-	ChargedToUser   User
-	ChargedToUserId uint `gorm:"not null"`
-	ReceiptId       uint `gorm:"not null"`
+	Name            string `json:"name" gorm:"not null"`
+	ChargedToUser   User   `json:"chargedToUser"`
+	ChargedToUserId uint   `json:"chargedToUserId" gorm:"not null"`
+	ReceiptId       uint   `json:"receiptId" gorm:"not null"`
 	Receipt         Receipt
 	Amount          decimal.Decimal `json:"amount" sql:"type:decimal(20,3);"`
 	IsTaxed         bool            `gorm:"not null"`

71d684ffa3cecc1e7db0aa6273708254b3134f6e
Noah <noah231515@gmail.com>
2022-11-15 08:51:06 -0500

    set up logic in middleware to check if token has been used

--- internal/middleware/refresh_token.go
+++ internal/middleware/refresh_token.go
@@ -3,28 +3,56 @@ package middleware
 import (
 	"context"
 	"net/http"
+	db "receipt-wrangler/api/internal/database"
+	"receipt-wrangler/api/internal/models"
 	"receipt-wrangler/api/internal/utils"
 )
 
 func ValidateRefreshToken(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		db := db.GetDB()
+		dbToken := models.RefreshToken{}
+
 		tokenValidator, err := utils.InitTokenValidator()
 		errMessage := "Error refreshing token"
 
 		if err != nil {
-			panic(err)
+			middleware_logger.Fatal(err.Error())
+			return
 		}
 
 		refreshTokenCookie, err := r.Cookie("refresh_token")
 		if err != nil {
 			utils.WriteCustomErrorResponse(w, errMessage, 500)
+			middleware_logger.Println("Refresh token cookie not found")
 			return
 		}
 
 		refreshToken, err := tokenValidator.ValidateToken(context.TODO(), refreshTokenCookie.Value)
 		if err != nil {
 			utils.WriteCustomErrorResponse(w, errMessage, 500)
+			middleware_logger.Println(err.Error())
 			return
+		}
+
+		err = db.Model(&models.RefreshToken{}).Where("token = ?", refreshTokenCookie.Value).Find(&dbToken).Error
+		if err != nil {
+			utils.WriteCustomErrorResponse(w, errMessage, 500)
+			middleware_logger.Println(err.Error())
+			return
+		}
+
+		if dbToken.IsUsed {
+			utils.WriteCustomErrorResponse(w, errMessage, 500)
+			middleware_logger.Println("Refresh token has been used already.", r, dbToken)
+			return
+		} else {
+			err = db.Model(&dbToken).Update("is_used", true).Error
+			if err != nil {
+				utils.WriteCustomErrorResponse(w, errMessage, 500)
+				middleware_logger.Println(err.Error())
+				return
+			}
 		}
 
 		ctx := context.WithValue(r.Context(), "refreshToken", refreshToken)

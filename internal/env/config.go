020cd22806c507c2f292917b5164c44d3b052f48
Noah <noah231515@gmail.com>
2022-11-27 11:29:51 -0500

    set up to grab config depending on env

--- internal/env/config.go
+++ internal/env/config.go
@@ -2,19 +2,29 @@ package config
 
 import (
 	"encoding/json"
+	"flag"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"receipt-wrangler/api/internal/structs"
+	"strings"
 )
 
 var config structs.Config
+var basePath string
 
 func GetConfig() structs.Config {
 	return config
 }
 
+func GetBasePath() string {
+	return basePath
+}
+
 func SetConfig() {
-	jsonFile, err := os.Open("config.json")
+	setBasePath()
+	env := getEnv()
+	jsonFile, err := os.Open(filepath.Join(basePath, "config."+env+".json"))
 
 	if err != nil {
 		panic(err.Error())
@@ -31,4 +41,25 @@ func SetConfig() {
 
 	jsonFile.Close()
 	config = configFile
+}
+
+func getEnv() string {
+	envFlag := flag.String("env", "dev", "set runtime environment")
+	flag.Parse()
+
+	return *envFlag
+}
+
+func setBasePath() {
+	cwd, _ := os.Getwd()
+	result := ""
+	paths := strings.Split(cwd, "/")
+
+	for i := 0; i < len(paths); i++ {
+		result += "/" + paths[i]
+		if paths[i] == "receipt-wrangler-api" {
+			basePath = result
+			return
+		}
+	}
 }

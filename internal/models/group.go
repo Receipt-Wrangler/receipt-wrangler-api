552ca427f77e42b646f62c4fd1824cd50cd2b015
Noah <noah231515@gmail.com>
2023-01-22 17:35:50 -0500

    fix name

--- internal/models/group.go
+++ internal/models/group.go
@@ -2,7 +2,7 @@ package models
 
 type Group struct {
 	BaseModel
-	Name           string      `gorm:"not null"`
+	Name           string      `gorm:"not null" json:"name"`
 	IsDefaultGroup bool        `json:"isDefault"`
 	GroupMembers   GroupMember `json:"groupMembers"`
 }

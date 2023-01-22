994d5567fbdc9309fadb51e6d7acb05433c120c7
Noah <noah231515@gmail.com>
2023-01-22 10:39:09 -0500

    create group when use signs up

--- internal/models/group_member.go
+++ internal/models/group_member.go
@@ -5,7 +5,6 @@ import "time"
 type GroupMember struct {
 	CreatedAt time.Time `json:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"`
-	Name      string    `gorm:"not null" json:"name"`
 	UserID    uint64    `gorm:"primaryKey;autoIncrement:false" json:"userId"`
 	GroupID   uint64    `gorm:"primaryKey;autoIncrement:false" json:"groupId"`
 }

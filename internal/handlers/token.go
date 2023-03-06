381b9a1e2a9349332f257c3b21979dc9af99a14c
Noah <noah231515@gmail.com>
2023-03-06 08:51:02 -0500

    refactor

--- internal/handlers/token.go
+++ internal/handlers/token.go
@@ -2,35 +2,46 @@ package handlers
 
 import (
 	"net/http"
+	"receipt-wrangler/api/internal/constants"
 	"receipt-wrangler/api/internal/services"
+	"receipt-wrangler/api/internal/structs"
 	"receipt-wrangler/api/internal/utils"
 
 	"github.com/auth0/go-jwt-middleware/v2/validator"
 )
 
 func RefreshToken(w http.ResponseWriter, r *http.Request) {
-	oldRefreshToken := r.Context().Value("refreshToken").(*validator.ValidatedClaims).CustomClaims.(*utils.Claims)
+	handler := structs.Handler{
+		ErrorMessage: "Error refreshing token",
+		Writer:       w,
+		Request:      r,
+		ResponseType: constants.APPLICATION_JSON,
+		HandlerFunction: func(w http.ResponseWriter, r *http.Request) (int, error) {
 
-	jwt, refreshToken, accessTokenClaims, err := utils.GenerateJWT(oldRefreshToken.UserId)
-	if err != nil {
-		handler_logger.Print(err.Error())
-		utils.WriteErrorResponse(w, err, 500)
-		return
+			oldRefreshToken := r.Context().Value("refreshToken").(*validator.ValidatedClaims).CustomClaims.(*utils.Claims)
+
+			jwt, refreshToken, accessTokenClaims, err := utils.GenerateJWT(oldRefreshToken.UserId)
+			if err != nil {
+				return http.StatusInternalServerError, err
+			}
+
+			services.PrepareAccessTokenClaims(accessTokenClaims)
+			bytes, err := utils.MarshalResponseData(accessTokenClaims)
+			if err != nil {
+				return http.StatusInternalServerError, err
+			}
+
+			accessTokenCookie, refreshTokenCookie := services.BuildTokenCookies(jwt, refreshToken)
+
+			http.SetCookie(w, &accessTokenCookie)
+			http.SetCookie(w, &refreshTokenCookie)
+
+			w.WriteHeader(200)
+			w.Write(bytes)
+
+			return 0, nil
+		},
 	}
 
-	services.PrepareAccessTokenClaims(accessTokenClaims)
-	bytes, err := utils.MarshalResponseData(accessTokenClaims)
-	if err != nil {
-		handler_logger.Print(err.Error())
-		utils.WriteErrorResponse(w, err, 500)
-		return
-	}
-
-	accessTokenCookie, refreshTokenCookie := services.BuildTokenCookies(jwt, refreshToken)
-
-	http.SetCookie(w, &accessTokenCookie)
-	http.SetCookie(w, &refreshTokenCookie)
-
-	w.WriteHeader(200)
-	w.Write(bytes)
+	HandleRequest(handler)
 }

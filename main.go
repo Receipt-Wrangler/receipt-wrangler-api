10f0c71de3907294d9d93e29aa00a6079cd7ebce
Noah <noah231515@gmail.com>
2023-03-07 08:59:22 -0500

    fix user endpoint resetting password

--- main.go
+++ main.go
@@ -133,7 +133,7 @@ func initRoutes() *chi.Mux {
 	userRouter.Get("/{username}", handlers.GetUsernameCount)
 	userRouter.With(middleware.SetUserData, middleware.ValidateRole(models.ADMIN), middleware.ValidateUserData(true)).Post("/", handlers.CreateUser)
 	userRouter.With(middleware.SetUserData, middleware.ValidateRole(models.ADMIN)).Post("/{id}", handlers.UpdateUser)
-	userRouter.With(middleware.SetResetPasswordData, middleware.ValidateRole(models.ADMIN)).Post("/{id}", handlers.ResetPassword)
+	userRouter.With(middleware.SetResetPasswordData, middleware.ValidateRole(models.ADMIN)).Post("/{id}/resetPassword", handlers.ResetPassword)
 	userRouter.With(middleware.ValidateRole(models.ADMIN)).Delete("/{id}", handlers.DeleteUser)
 	userRouter.With(middleware.ValidateGroupRole(models.VIEWER)).Get("/amountOwedForUser/{groupId}", handlers.GetAmountOwedForUser)
 	userRouter.Get("/getUserClaims", handlers.GetClaimsForLoggedInUser)

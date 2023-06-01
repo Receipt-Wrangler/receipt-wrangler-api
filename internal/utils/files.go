8207d917fe4f6624e68f6ca38dc1383815132c5a
Noah <noah231515@gmail.com>
2023-06-01 09:55:22 -0400

    create dir on group create

--- internal/utils/files.go
+++ internal/utils/files.go
@@ -8,33 +8,48 @@ import (
 	"receipt-wrangler/api/internal/models"
 )
 
-func BuildFilePath(rid string, fId string, fname string) (string, error) {
+func BuildFilePath(rid string, fid string, fname string) (string, error) {
 	db := db.GetDB()
 	var receipt models.Receipt
-	var group models.Group
 
 	err := db.Model(models.Receipt{}).Where("id = ?", rid).Select("group_id").Find(&receipt).Error
 	if err != nil {
 		return "", err
 	}
 
+	groupPath, err := BuildGroupPath(receipt.GroupId)
+	if err != nil {
+		return "", err
+	}
+
+	fileName := BuildFileName(rid, fid, fname)
+	path := filepath.Join(groupPath, fileName)
+
+	return path, nil
+}
+
+func BuildFileName(rid string, fid string, fname string) string {
+	return rid + "-" + fid + "-" + fname
+}
+
+func BuildGroupPath(groupId uint) (string, error) {
+	db := db.GetDB()
+	var group models.Group
+
 	basePath, err := os.Getwd()
 	if err != nil {
 		return "", err
 	}
 
-	err = db.Model(models.Group{}).Where("id = ?", receipt.GroupId).Select("id", "name").Find(&group).Error
+	err = db.Model(models.Group{}).Where("id = ?", groupId).Select("id", "name").Find(&group).Error
 	if err != nil {
 		return "", err
 	}
 
 	strGroupId := UintToString(group.ID)
-
-	fileName := rid + "-" + fId + "-" + fname
 	groupPath := strGroupId + "-" + group.Name
-	path := filepath.Join(basePath, "data", groupPath, fileName)
 
-	return path, nil
+	return filepath.Join(basePath, "data", groupPath), nil
 }
 
 func WriteFile(path string, data []byte) error {
@@ -73,13 +88,22 @@ func GetBytesForFileData(fileData models.FileData) ([]byte, error) {
 func DirectoryExists(dir string, createIfNotExist bool) error {
 	_, err := os.Stat(dir)
 	if errors.Is(err, os.ErrNotExist) && createIfNotExist {
-		err = os.Mkdir(dir, os.ModePerm)
+		err = MakeDirectory(dir)
 		if err != nil {
 			return err
 		}
 	}
 
 	if !errors.Is(err, os.ErrNotExist) {
+		return err
+	}
+
+	return nil
+}
+
+func MakeDirectory(dir string) error {
+	err := os.Mkdir(dir, os.ModePerm)
+	if err != nil {
 		return err
 	}
 

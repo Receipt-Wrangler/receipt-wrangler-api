39fe634c1d4293f6998eca4c79414e2f74498e8b
Noah <noah231515@gmail.com>
2023-07-08 14:55:04 -0400

    unit tests

--- internal/repositories/comments_test.go
+++ internal/repositories/comments_test.go
@@ -1,6 +1,7 @@
 package repositories
 
 import (
+	"fmt"
 	db "receipt-wrangler/api/internal/database"
 	"receipt-wrangler/api/internal/models"
 	"receipt-wrangler/api/internal/utils"
@@ -12,7 +13,7 @@ var commentRepository CommentRepository
 func setupCommentTest() {
 	utils.CreateTestGroupWithUsers()
 	createTestReceipt()
-	commentRepository = NewCommentRepository(db.GetDB(), nil)
+	commentRepository = NewCommentRepository(nil)
 }
 
 func createTestReceipt() {
@@ -27,13 +28,15 @@ func createTestReceipt() {
 
 func teardownCommentTest() {
 	db := db.GetDB()
+	utils.TruncateTable(db, "notifications")
 	utils.TruncateTable(db, "comments")
 	utils.TruncateTable(db, "receipts")
+	utils.TruncateTable(db, "group_members")
 	utils.TruncateTable(db, "groups")
 	utils.TruncateTable(db, "users")
 }
 
-func TestShouldAddComment(t *testing.T) {
+func TestShouldAddCommentAndSendNotificationToAllGroupUsers(t *testing.T) {
 	setupCommentTest()
 	userId := uint(1)
 	comment := models.Comment{
@@ -42,10 +45,83 @@ func TestShouldAddComment(t *testing.T) {
 		UserId:    &userId,
 	}
 
-	_, err := commentRepository.AddComment(comment)
+	newComment, err := commentRepository.AddComment(comment)
 	if err != nil {
 		utils.PrintTestError(t, err, nil)
 		return
+	}
+
+	if newComment.ID != 1 {
+		utils.PrintTestError(t, newComment.ID, 1)
+	}
+
+	notificationRepository := NewNotificationRepository(nil)
+
+	user1Notifications, _ := notificationRepository.GetNotificationsForUser(1)
+	if len(user1Notifications) > 0 {
+		utils.PrintTestError(t, len(user1Notifications), 0)
+	}
+
+	user2Notifications, _ := notificationRepository.GetNotificationsForUser(2)
+	if len(user2Notifications) != 1 {
+		utils.PrintTestError(t, len(user2Notifications), 1)
+	}
+
+	user3Notifications, _ := notificationRepository.GetNotificationsForUser(3)
+	if len(user3Notifications) != 1 {
+		utils.PrintTestError(t, len(user3Notifications), 1)
+	}
+
+	teardownCommentTest()
+}
+
+func TestShouldAddCommentAndSendNotificationToThreadUsers(t *testing.T) {
+	fmt.Println("test 2")
+	db := db.GetDB()
+	setupCommentTest()
+	user1Id := uint(1)
+	user2Id := uint(2)
+
+	parentComment := models.Comment{
+		Comment:   "test",
+		ReceiptId: 1,
+		UserId:    &user1Id,
+	}
+	db.Create(&parentComment)
+
+	comment := models.Comment{
+		Comment:   "test2",
+		ReceiptId: 1,
+		UserId:    &user2Id,
+		CommentId: &parentComment.ID,
+	}
+
+	newComment, err := commentRepository.AddComment(comment)
+	if err != nil {
+		utils.PrintTestError(t, err, nil)
+		return
+	}
+
+	if newComment.ID != 2 {
+		utils.PrintTestError(t, newComment.ID, 2)
+		return
+	}
+
+	notificationRepository := NewNotificationRepository(nil)
+
+	user1Notifications, _ := notificationRepository.GetNotificationsForUser(1)
+	if len(user1Notifications) != 1 {
+		utils.PrintTestError(t, len(user1Notifications), 1)
+	}
+
+	user2Notifications, _ := notificationRepository.GetNotificationsForUser(2)
+	if len(user2Notifications) != 0 {
+		utils.PrintTestError(t, len(user2Notifications), 0)
+	}
+
+	user3Notifications, _ := notificationRepository.GetNotificationsForUser(3)
+	if len(user3Notifications) != 0 {
+		utils.PrintTestError(t, len(user3Notifications), 0)
 	}
 
 	teardownCommentTest()

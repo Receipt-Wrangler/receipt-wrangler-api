2e90a0cc1569285408d0125996741146fb9ff671
Noah <noah231515@gmail.com>
2023-08-11 16:27:33 -0400

    fix user

--- internal/routers/user.go
+++ internal/routers/user.go
@@ -13,17 +13,19 @@ import (
 func BuildUserRouter(tokenValidator *jwtmiddleware.JWTMiddleware) *chi.Mux {
 	userRouter := chi.NewRouter()
 
-	userRouter.Use(middleware.MoveJWTCookieToHeader, tokenValidator.CheckJWT)
-	userRouter.Get("/", handlers.GetAllUsers)
+	// Authenticated routes
+	userRouter.With(middleware.MoveJWTCookieToHeader,tokenValidator.CheckJWT).Get("/", handlers.GetAllUsers)
+	userRouter.With(middleware.MoveJWTCookieToHeader,tokenValidator.CheckJWT, middleware.SetGeneralBodyData("updateProfileCommand", commands.UpdateProfileCommand{})).Put("/updateUserProfile", handlers.UpdateUserProfile)
+	userRouter.With(middleware.MoveJWTCookieToHeader,tokenValidator.CheckJWT,middleware.SetUserData, middleware.ValidateRole(models.ADMIN), middleware.ValidateUserData(true)).Post("/", handlers.CreateUser)
+	userRouter.With(middleware.MoveJWTCookieToHeader,tokenValidator.CheckJWT,middleware.SetUserData, middleware.ValidateRole(models.ADMIN)).Put("/{id}", handlers.UpdateUser)
+	userRouter.With(middleware.MoveJWTCookieToHeader,tokenValidator.CheckJWT,middleware.SetResetPasswordData, middleware.ValidateRole(models.ADMIN)).Post("/{id}/resetPassword", handlers.ResetPassword)
+	userRouter.With(middleware.MoveJWTCookieToHeader,tokenValidator.CheckJWT,middleware.SetResetPasswordData, middleware.ValidateRole(models.ADMIN)).Post("/{id}/convertDummyUserToNormalUser", handlers.ConvertDummyUserToNormalUser)
+	userRouter.With(middleware.MoveJWTCookieToHeader,tokenValidator.CheckJWT, middleware.ValidateRole(models.ADMIN)).Delete("/{id}", handlers.DeleteUser)
+	userRouter.With(middleware.MoveJWTCookieToHeader,tokenValidator.CheckJWT).Get("/amountOwedForUser", handlers.GetAmountOwedForUser)
+	userRouter.With(middleware.MoveJWTCookieToHeader,tokenValidator.CheckJWT).Get("/getUserClaims", handlers.GetClaimsForLoggedInUser)
+	
+	// Unauthenticated routes
 	userRouter.Get("/{username}", handlers.GetUsernameCount)
-	userRouter.With(middleware.SetGeneralBodyData("updateProfileCommand", commands.UpdateProfileCommand{})).Put("/updateUserProfile", handlers.UpdateUserProfile)
-	userRouter.With(middleware.SetUserData, middleware.ValidateRole(models.ADMIN), middleware.ValidateUserData(true)).Post("/", handlers.CreateUser)
-	userRouter.With(middleware.SetUserData, middleware.ValidateRole(models.ADMIN)).Put("/{id}", handlers.UpdateUser)
-	userRouter.With(middleware.SetResetPasswordData, middleware.ValidateRole(models.ADMIN)).Post("/{id}/resetPassword", handlers.ResetPassword)
-	userRouter.With(middleware.SetResetPasswordData, middleware.ValidateRole(models.ADMIN)).Post("/{id}/convertDummyUserToNormalUser", handlers.ConvertDummyUserToNormalUser)
-	userRouter.With(middleware.ValidateRole(models.ADMIN)).Delete("/{id}", handlers.DeleteUser)
-	userRouter.Get("/amountOwedForUser", handlers.GetAmountOwedForUser)
-	userRouter.Get("/getUserClaims", handlers.GetClaimsForLoggedInUser)
 
 	return userRouter
 }

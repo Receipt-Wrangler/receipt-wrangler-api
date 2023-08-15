5e91b6c9ec55a349c19d00e2b8e36500542619fd
Noah <noah231515@gmail.com>
2023-08-14 21:04:58 -0400

    add group id adn route

--- internal/structs/handler.go
+++ internal/structs/handler.go
@@ -1,11 +1,16 @@
 package structs
 
-import "net/http"
+import (
+	"net/http"
+	"receipt-wrangler/api/internal/models"
+)
 
 type Handler struct {
 	ErrorMessage    string
 	Writer          http.ResponseWriter
 	Request         *http.Request
+	GroupRole       models.GroupRole
+	GroupId					string
 	HandlerFunction func(http.ResponseWriter, *http.Request) (int, error)
 	ResponseType    string
 }

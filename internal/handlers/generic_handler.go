d3fd7b8aa2d7ddf2b9246eb20d536a40c240b986
Noah <noah231515@gmail.com>
2023-08-14 21:04:09 -0400

    add group role check to generic handler

--- internal/handlers/generic_handler.go
+++ internal/handlers/generic_handler.go
@@ -1,12 +1,25 @@
 package handlers
 
 import (
+	"net/http"
+	"receipt-wrangler/api/internal/models"
+	"receipt-wrangler/api/internal/services"
+	"receipt-wrangler/api/internal/simpleutils"
 	"receipt-wrangler/api/internal/structs"
 	"receipt-wrangler/api/internal/utils"
 )
 
 func HandleRequest(handler structs.Handler) {
 	errCode, err := handler.HandlerFunction(handler.Writer, handler.Request)
+	if len(handler.GroupRole) > 0 && len(handler.GroupId) > 0 {
+		token := utils.GetJWT(handler.Request)
+		err := services.ValidateGroupRole(models.GroupRole(handler.GroupRole), handler.GroupId, simpleutils.UintToString(token.UserId))
+		if err != nil {
+			handler_logger.Print(err.Error())
+			utils.WriteCustomErrorResponse(handler.Writer, "User is unauthorized to access entity", http.StatusForbidden)
+			return
+		}
+	}
 	if err != nil {
 		handler_logger.Print(err.Error())
 		utils.WriteCustomErrorResponse(handler.Writer, handler.ErrorMessage, errCode)

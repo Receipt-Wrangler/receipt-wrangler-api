9e3fa28481e0a6d0634fd0e7eacb5d4cbb830d2c
Noah <noah231515@gmail.com>
2023-08-22 08:48:28 -0400

    create if not

--- internal/repositories/user_preferences.go
+++ internal/repositories/user_preferences.go
@@ -2,6 +2,7 @@ package repositories
 
 import (
 	"receipt-wrangler/api/internal/models"
+	"receipt-wrangler/api/internal/simpleutils"
 
 	"gorm.io/gorm"
 )
@@ -23,6 +24,33 @@ func (repository UserPreferncesRepository) GetUserPreferencesOrCreate(userId str
 	var userPreferences models.UserPrefernces
 
 	err := db.Model(models.UserPrefernces{}).Where("user_id = ?", userId).Find(&userPreferences).Error
+	if err != nil {
+		return models.UserPrefernces{}, err
+	}
+
+	if userPreferences.ID == 0 {
+		uintUserId, err := simpleutils.StringToUint(userId)
+		if err != nil {
+			return models.UserPrefernces{}, err
+		}
+
+		userPreferencesToCreate := models.UserPrefernces{
+			UserId: uintUserId,
+		}
+
+		userPreferences, err = repository.CreateUserPreferences(userPreferencesToCreate)
+		if err != nil {
+			return models.UserPrefernces{}, err
+		}
+	}
+
+	return userPreferences, nil
+}
+
+func (repository UserPreferncesRepository) CreateUserPreferences(userPreferences models.UserPrefernces) (models.UserPrefernces, error) {
+	db := repository.GetDB()
+
+	err := db.Model(models.UserPrefernces{}).Create(&userPreferences).Error
 	if err != nil {
 		return models.UserPrefernces{}, err
 	}

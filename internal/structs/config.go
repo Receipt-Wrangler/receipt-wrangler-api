65381e1a6fe1bd67fc914f9b9a3f0f2293d86bd0
Noah <noah231515@gmail.com>
2023-09-06 08:06:57 -0400

    read in email settings

--- internal/structs/config.go
+++ internal/structs/config.go
@@ -1,6 +1,14 @@
 package structs
 
+type EmailSettings struct {
+	Host     string
+	Port     int
+	Username string
+	Password string
+}
+
 type Config struct {
-	SecretKey string
-	OpenAiKey string
+	SecretKey     string
+	OpenAiKey     string
+	EmailSettings []EmailSettings
 }

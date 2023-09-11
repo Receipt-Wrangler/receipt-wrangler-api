c730caec527b24507de84bbcadba9ab234c11323
Noah <noah231515@gmail.com>
2023-09-11 08:22:43 -0400

    add enabled

--- internal/models/group_settings.go
+++ internal/models/group_settings.go
@@ -2,8 +2,9 @@ package models
 
 type GroupSettings struct {
 	BaseModel
-	GroupId            uint                 `gorm:"not null;unique" json:"groupId"`
-	EmailToRead        GroupSettingsEmail   `json:"emailToRead"`
-	SubjectLineRegexes []SubjectLineRegex   `json:"subjectLineRegexes"`
-	EmailWhiteList     []GroupSettingsEmail `json:"emailWhiteList"`
+	GroupId                 uint                 `gorm:"not null;unique" json:"groupId"`
+	EmailIntegrationEnabled bool                 `gorm:"not null; default:false" json:"emailIntegrationEnabled"`
+	EmailToRead             GroupSettingsEmail   `json:"emailToRead"`
+	SubjectLineRegexes      []SubjectLineRegex   `json:"subjectLineRegexes"`
+	EmailWhiteList          []GroupSettingsEmail `json:"emailWhiteList"`
 }

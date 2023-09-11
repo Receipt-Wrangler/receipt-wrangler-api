65d1d00e7b8082886cd14e8c95c56a27028300e4
Noah <noah231515@gmail.com>
2023-09-10 22:59:07 -0400

    set up association replacements

--- internal/repositories/group_settings.go
+++ internal/repositories/group_settings.go
@@ -1,11 +1,11 @@
 package repositories
 
 import (
+	"fmt"
 	"receipt-wrangler/api/internal/commands"
 	"receipt-wrangler/api/internal/models"
 
 	"gorm.io/gorm"
-	"gorm.io/gorm/clause"
 )
 
 type GroupSettingsRepository struct {
@@ -31,19 +31,39 @@ func (repository GroupSettingsRepository) CreateGroupSettings(groupSettings mode
 	return groupSettings, nil
 }
 
-func (repository GroupSettingsRepository) UpdateGroupSettings(groupId string, groupSettings commands.UpdateGroupSettingsCommand) (models.GroupSettings, error) {
+func (repository GroupSettingsRepository) UpdateGroupSettings(groupId string, command commands.UpdateGroupSettingsCommand) (models.GroupSettings, error) {
 	db := repository.GetDB()
-	var result models.GroupSettings
+	var groupSettings models.GroupSettings
 
-	err := db.Model(&models.GroupSettings{}).Where("group_id = ?", groupId).Clauses(clause.OnConflict{UpdateAll: true}).Create(&groupSettings).Error
+	err := db.Model(&models.GroupSettings{}).Where("group_id = ?", groupId).First(&groupSettings).Error
 	if err != nil {
 		return models.GroupSettings{}, err
 	}
 
-	err = db.Model(&models.GroupSettings{}).Where("group_id = ?", groupId).First(&result).Error
+	err = db.Transaction(func(tx *gorm.DB) error {
+
+		err = tx.Model(&groupSettings).Where("id = ?", groupSettings.ID).Association("EmailToRead").Replace(&command.EmailToRead)
+		if err != nil {
+			fmt.Println(err.Error())
+			return err
+		}
+
+		err = tx.Model(&groupSettings).Where("id = ?", groupSettings.ID).Association("SubjectLineRegexes").Replace(&command.SubjectLineRegexes)
+		if err != nil {
+			return err
+		}
+
+		err = tx.Model(&groupSettings).Where("id = ?", groupSettings.ID).Association("EmailWhiteList").Replace(&command.EmailWhiteList)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+
 	if err != nil {
 		return models.GroupSettings{}, err
 	}
 
-	return result, nil
+	return groupSettings, nil
 }

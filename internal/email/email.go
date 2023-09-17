3a611a8f9325be2db493737d75e3aeb5959eb2c9
Noah <noah231515@gmail.com>
2023-09-17 01:03:31 -0400

    process emails

--- internal/email/email.go
+++ internal/email/email.go
@@ -4,16 +4,23 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"os"
 	"os/exec"
 	config "receipt-wrangler/api/internal/env"
 	"receipt-wrangler/api/internal/logging"
+	"receipt-wrangler/api/internal/models"
 	"receipt-wrangler/api/internal/repositories"
+	"receipt-wrangler/api/internal/services"
 	"receipt-wrangler/api/internal/structs"
+	"receipt-wrangler/api/internal/utils"
+
+	"gorm.io/gorm"
 )
 
 func PollEmails() error {
 	err := callClient()
 	if err != nil {
+		logging.GetLogger().Println(err.Error())
 		fmt.Println(err.Error())
 	}
 
@@ -57,7 +64,7 @@ func callClient() error {
 
 	fmt.Println(result)
 
-	err = processEmails()
+	err = processEmails(result)
 	if err != nil {
 		logger.Println(err.Error())
 		return err
@@ -66,6 +73,69 @@ func callClient() error {
 	return nil
 }
 
-func processEmails() error {
+func processEmails(emailMetadata []structs.EmailMetadata) error {
+	basePath := config.GetBasePath() + "/temp"
+	db := repositories.GetDB()
+	imagesToRemove := []string{}
+
+	err := db.Transaction(func(tx *gorm.DB) error {
+		receiptRepository := repositories.NewReceiptRepository(tx)
+		receiptImageRepository := repositories.NewReceiptImageRepository(tx)
+
+		for _, metadata := range emailMetadata {
+			for _, attachment := range metadata.Attachments {
+				path := basePath + "/" + attachment.Filename
+				receipt, err := services.ReadReceiptImageFromFileOnly(path)
+				if err != nil {
+					return err
+				}
+
+				for _, groupId := range metadata.GroupIds {
+					receipt.GroupId = groupId
+					receipt.Status = models.DRAFT
+					receipt.PaidByUserID = 1
+
+					// TODO: Add user id
+					createdReceipt, err := receiptRepository.CreateReceipt(receipt, 1)
+					if err != nil {
+						return err
+					}
+
+					bytes, err := utils.ReadFile(path)
+					if err != nil {
+						return err
+					}
+
+					fileData := models.FileData{
+						ReceiptId: createdReceipt.ID,
+						Name:      attachment.Filename,
+						FileType:  attachment.FileType,
+						Size:      attachment.Size,
+						ImageData: bytes,
+					}
+
+					_, err = receiptImageRepository.CreateReceiptImage(fileData)
+					if err != nil {
+						return err
+					}
+
+				}
+
+				imagesToRemove = append(imagesToRemove, path)
+			}
+		}
+
+		return nil
+	})
+
+	for _, path := range imagesToRemove {
+		os.Remove(path)
+	}
+
+	if err != nil {
+		logging.GetLogger().Println(err.Error())
+		return err
+	}
+
 	return nil
 }

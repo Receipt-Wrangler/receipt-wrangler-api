f1774c94a61f76f6e80a84a80324e507ea69c9f7
Noah <noah231515@gmail.com>
2023-10-04 08:45:14 -0400

    add switch scripts

---
+++ dev/switch-to-mariadb.sh
@@ -0,0 +1,6 @@
+export DB_HOST=0.0.0.0:3306
+export DB_USER=wrangler
+export DB_NAME=wrangler
+export DB_ENGINE=mariadb
+export DB_PASSWORD=123456
+  
---
+++ dev/switch-to-postgresql.sh
@@ -0,0 +1,6 @@
+export DB_HOST=0.0.0.0
+export DB_PORT=5432
+export DB_USER=wrangler
+export DB_NAME=wrangler
+export DB_PASSWORD=123456
+export DB_ENIGNES=postgresql

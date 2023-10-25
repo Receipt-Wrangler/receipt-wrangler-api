a7626960f1f087c0b33c44a092d6b4f35a5b032b
Noah <noah231515@gmail.com>
2023-10-25 08:43:01 -0400

    random string func

---
+++ internal/utils/string.go
@@ -0,0 +1,16 @@
+package utils
+
+import (
+	"encoding/base64"
+	"math/rand"
+)
+
+func GetRandomString(length int) (string, error) {
+	bytes := make([]byte, length)
+	_, err := rand.Read(bytes)
+	if err != nil {
+		return "", err
+	}
+
+	return base64.URLEncoding.EncodeToString(bytes), nil
+}

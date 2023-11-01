e9940b3150d1f62a625bfd4ea2a93181e1b33bc0
Noah <noah231515@gmail.com>
2023-11-01 08:51:01 -0400

    add command data load

--- internal/commands/dashboard_upsert_command.go
+++ internal/commands/dashboard_upsert_command.go
@@ -1,8 +1,27 @@
 package commands
 
-import "receipt-wrangler/api/internal/models"
+import (
+	"encoding/json"
+	"net/http"
+	"receipt-wrangler/api/internal/models"
+	"receipt-wrangler/api/internal/utils"
+)
 
 type UpsertDashboardCommand struct {
 	Name    string          `json:"name"`
 	Widgets []models.Widget `json:"widgets"`
+}
+
+func (tag *UpsertDashboardCommand) LoadDataFromRequest(w http.ResponseWriter, r *http.Request) error {
+	bytes, err := utils.GetBodyData(w, r)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(bytes, &tag)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }

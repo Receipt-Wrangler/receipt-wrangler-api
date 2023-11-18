fd1175fc9ff16db940673804118c4f27c3b77ab3
Noah <noah231515@gmail.com>
2023-11-18 11:04:34 -0500

    add ionic serve

--- internal/corspolicy/cors.go
+++ internal/corspolicy/cors.go
@@ -10,7 +10,7 @@ func GetCorsPolicy() *cors.Cors {
 	env := config.GetDeployEnv()
 	if env == "dev" {
 		return cors.New(cors.Options{
-			AllowedOrigins:      []string{"http://localhost:4200"},
+			AllowedOrigins:      []string{"http://localhost:4200", "http://localhost:8100"},
 			AllowCredentials:    true,
 			AllowPrivateNetwork: true,
 			AllowedMethods:      []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},

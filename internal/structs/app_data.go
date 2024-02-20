4a038d79c202a3889049de4a59dd78eaab42993b
Noah <noah231515@gmail.com>
2024-02-20 08:27:43 -0500

    get categories and tags in app data

--- internal/structs/app_data.go
+++ internal/structs/app_data.go
@@ -3,9 +3,11 @@ package structs
 import "receipt-wrangler/api/internal/models"
 
 type AppData struct {
-	Claims Claims         `json:"claims"`
-	Groups []models.Group `json:"groups"`
 	TokenPair
+	Claims          Claims                `json:"claims"`
+	Groups          []models.Group        `json:"groups"`
+	Categories      []models.Category     `json:"categories"`
+	Tags            []models.Tag          `json:"tags"`
 	UserPreferences models.UserPrefernces `json:"userPreferences"`
 	Users           []UserView            `json:"users"`
 	FeatureConfig   FeatureConfig         `json:"featureConfig"`

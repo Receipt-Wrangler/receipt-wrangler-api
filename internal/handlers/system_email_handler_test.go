eccdb217f26493b10ac867078e241ef254e6c554
Noah <navi0001@protonmail.com>
2024-04-28 10:24:01 -0400

    refactor tests to table

--- internal/handlers/system_email_handler_test.go
+++ internal/handlers/system_email_handler_test.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -179,142 +180,65 @@ func TestShouldNotAllowUserToGetSystemEmails(t *testing.T) {
 	}
 }
 
-func TestShouldReturn500WithMalformedPagedRequestCommand1(t *testing.T) {
+func TestShouldReturn500WithMalformedPagedRequestCommand(t *testing.T) {
 	defer tearDownSystemEmailTest()
 
-	pagedRequestCommand := commands.PagedRequestCommand{
-		Page:          1,
-		PageSize:      50,
-		OrderBy:       "badOrderBy",
-		SortDirection: "asc",
-	}
-	bytes, _ := json.Marshal(pagedRequestCommand)
-
-	reader := strings.NewReader(string(bytes))
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest("POST", "/api", reader)
-
-	newContext := context.WithValue(r.Context(), jwtmiddleware.ContextKey{}, &validator.ValidatedClaims{CustomClaims: &structs.Claims{UserId: 1, UserRole: models.ADMIN}})
-	r = r.WithContext(newContext)
-	repositories.CreateTestUser()
-
-	GetAllSystemEmails(w, r)
-
-	if w.Result().StatusCode != http.StatusInternalServerError {
-		utils.PrintTestError(t, w.Result().StatusCode, http.StatusInternalServerError)
-	}
-}
-
-func TestShouldReturn500WithMalformedPagedRequestCommand2(t *testing.T) {
-	defer tearDownSystemEmailTest()
-
-	pagedRequestCommand := commands.PagedRequestCommand{
-		Page:          1,
-		PageSize:      50,
-		OrderBy:       "host",
-		SortDirection: "badSortDirection",
-	}
-	bytes, _ := json.Marshal(pagedRequestCommand)
-
-	reader := strings.NewReader(string(bytes))
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest("POST", "/api", reader)
-
-	newContext := context.WithValue(r.Context(), jwtmiddleware.ContextKey{}, &validator.ValidatedClaims{CustomClaims: &structs.Claims{UserId: 1, UserRole: models.ADMIN}})
-	r = r.WithContext(newContext)
-	repositories.CreateTestUser()
-
-	GetAllSystemEmails(w, r)
-
-	if w.Result().StatusCode != http.StatusBadRequest {
-		utils.PrintTestError(t, w.Result().StatusCode, http.StatusBadRequest)
-	}
-}
-
-func TestShouldReturn500WithMalformedPagedRequestCommand3(t *testing.T) {
-	defer tearDownSystemEmailTest()
-
-	pagedRequestCommand := commands.PagedRequestCommand{
-		Page:          -1,
-		PageSize:      50,
-		OrderBy:       "host",
-		SortDirection: "asc",
-	}
-	bytes, _ := json.Marshal(pagedRequestCommand)
-
-	reader := strings.NewReader(string(bytes))
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest("POST", "/api", reader)
-
-	newContext := context.WithValue(r.Context(), jwtmiddleware.ContextKey{}, &validator.ValidatedClaims{CustomClaims: &structs.Claims{UserId: 1, UserRole: models.ADMIN}})
-	r = r.WithContext(newContext)
-	repositories.CreateTestUser()
-
-	GetAllSystemEmails(w, r)
-
-	if w.Result().StatusCode != http.StatusBadRequest {
-		utils.PrintTestError(t, w.Result().StatusCode, http.StatusBadRequest)
-	}
-}
-
-func TestShouldReturn500WithMalformedPagedRequestCommand4(t *testing.T) {
-	defer tearDownSystemEmailTest()
-
-	pagedRequestCommand := commands.PagedRequestCommand{
-		Page:          1,
-		PageSize:      -1,
-		OrderBy:       "host",
-		SortDirection: "asc",
-	}
-	bytes, _ := json.Marshal(pagedRequestCommand)
-
-	reader := strings.NewReader(string(bytes))
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest("POST", "/api", reader)
-
-	newContext := context.WithValue(r.Context(), jwtmiddleware.ContextKey{}, &validator.ValidatedClaims{CustomClaims: &structs.Claims{UserId: 1, UserRole: models.ADMIN}})
-	r = r.WithContext(newContext)
-	repositories.CreateTestUser()
-
-	GetAllSystemEmails(w, r)
-
-	if w.Result().StatusCode != http.StatusBadRequest {
-		utils.PrintTestError(t, w.Result().StatusCode, http.StatusBadRequest)
-	}
-}
-
-func TestShouldAllowUserToGetEmptySystemEmails(t *testing.T) {
-	defer tearDownSystemEmailTest()
-
-	pagedRequestCommand := commands.PagedRequestCommand{
-		Page:          1,
-		PageSize:      50,
-		OrderBy:       "host",
-		SortDirection: "asc",
-	}
-	bytes, _ := json.Marshal(pagedRequestCommand)
-
-	reader := strings.NewReader(string(bytes))
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest("POST", "/api", reader)
-
-	newContext := context.WithValue(r.Context(), jwtmiddleware.ContextKey{}, &validator.ValidatedClaims{CustomClaims: &structs.Claims{UserId: 1, UserRole: models.ADMIN}})
-	r = r.WithContext(newContext)
-
-	repositories.CreateTestUser()
-
-	GetAllSystemEmails(w, r)
-
-	if w.Result().StatusCode != http.StatusOK {
-		utils.PrintTestError(t, w.Result().StatusCode, http.StatusOK)
+	tests := map[string]struct {
+		input  commands.PagedRequestCommand
+		expect int
+	}{
+		"badOrderBy": {
+			input: commands.PagedRequestCommand{
+				Page:          1,
+				PageSize:      50,
+				OrderBy:       "badOrderBy",
+				SortDirection: "asc",
+			},
+			expect: http.StatusInternalServerError,
+		},
+		"badSortDirection": {
+			input: commands.PagedRequestCommand{
+				Page:          1,
+				PageSize:      50,
+				OrderBy:       "host",
+				SortDirection: "badSortDirection",
+			},
+			expect: http.StatusBadRequest,
+		},
+		"badPage": {
+			input: commands.PagedRequestCommand{
+				Page:          -1,
+				PageSize:      50,
+				OrderBy:       "host",
+				SortDirection: "asc",
+			},
+			expect: http.StatusBadRequest,
+		},
+		"badPageSize": {
+			input: commands.PagedRequestCommand{
+				Page:          1,
+				PageSize:      -1,
+				OrderBy:       "host",
+				SortDirection: "asc",
+			},
+			expect: http.StatusBadRequest,
+		},
 	}
 
-	pagedData := structs.PagedData{}
-	response, _ := io.ReadAll(w.Body)
-	json.Unmarshal(response, &pagedData)
+	for name, test := range tests {
+		bytes, _ := json.Marshal(test.input)
+		reader := strings.NewReader(string(bytes))
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest("POST", "/api", reader)
 
-	if pagedData.TotalCount != 0 {
-		utils.PrintTestError(t, pagedData.TotalCount, 0)
+		newContext := context.WithValue(r.Context(), jwtmiddleware.ContextKey{}, &validator.ValidatedClaims{CustomClaims: &structs.Claims{UserId: 1, UserRole: models.ADMIN}})
+		r = r.WithContext(newContext)
+
+		GetAllSystemEmails(w, r)
+
+		if w.Result().StatusCode != test.expect {
+			utils.PrintTestError(t, w.Result().StatusCode, fmt.Sprintf("%s expected: %d", name, test.expect))
+		}
 	}
 }
 

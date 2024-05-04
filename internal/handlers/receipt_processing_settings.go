e1b0a02d5a72920251f2a8c32e6ebb358778632d
Noah <navi0001@protonmail.com>
2024-05-04 10:58:30 -0400

    implement handler, and unit tests

---
+++ internal/handlers/receipt_processing_settings.go
@@ -0,0 +1,59 @@
+package handlers
+
+import (
+	"net/http"
+	"receipt-wrangler/api/internal/commands"
+	"receipt-wrangler/api/internal/constants"
+	"receipt-wrangler/api/internal/models"
+	"receipt-wrangler/api/internal/repositories"
+	"receipt-wrangler/api/internal/structs"
+	"receipt-wrangler/api/internal/utils"
+)
+
+func GetPagedReceiptProcessingSettings(w http.ResponseWriter, r *http.Request) {
+	handler := structs.Handler{
+		ErrorMessage: "Error getting receipt processing settings",
+		Writer:       w,
+		Request:      r,
+		UserRole:     models.ADMIN,
+		ResponseType: constants.APPLICATION_JSON,
+		HandlerFunction: func(w http.ResponseWriter, r *http.Request) (int, error) {
+			command := commands.PagedRequestCommand{}
+			err := command.LoadDataFromRequest(w, r)
+			if err != nil {
+				return http.StatusInternalServerError, err
+			}
+
+			vErr := command.Validate()
+			if len(vErr.Errors) > 0 {
+				structs.WriteValidatorErrorResponse(w, vErr, http.StatusBadRequest)
+				return 0, nil
+			}
+
+			receiptProcessingSettingsRepository := repositories.NewReceiptProcessingSettings(nil)
+			receiptProcessingSettings, count, err := receiptProcessingSettingsRepository.GetPagedReceiptProcessingSettings(command)
+			if err != nil {
+				return http.StatusInternalServerError, err
+			}
+
+			pagedData := structs.PagedData{}
+			data := make([]interface{}, 0)
+			for i := 0; i < len(receiptProcessingSettings); i++ {
+				data = append(data, receiptProcessingSettings[i])
+			}
+			pagedData.TotalCount = count
+
+			responseBytes, err := utils.MarshalResponseData(pagedData)
+			if err != nil {
+				return http.StatusInternalServerError, err
+			}
+
+			w.WriteHeader(http.StatusOK)
+			w.Write(responseBytes)
+
+			return 0, nil
+		},
+	}
+
+	HandleRequest(handler)
+}

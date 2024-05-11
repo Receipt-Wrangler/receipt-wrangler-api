940b3b95731a73a84b4743e93fc8a8071a38acae
Noah <navi0001@protonmail.com>
2024-05-11 11:36:11 -0400

    get paged

--- internal/models/receipt_processing_settings.go
+++ internal/models/receipt_processing_settings.go
@@ -8,15 +8,16 @@ import (
 
 type ReceiptProcessingSettings struct {
 	BaseModel
-	Name       string       `gorm:"not null; uniqueIndex" json:"name"`
-	AiType     AiClientType `json:"type"`
-	Url        string       `json:"url"`
-	Key        string       `json:"key"`
-	Model      string       `json:"model"`
-	NumWorkers int          `json:"numWorkers"`
-	OcrEngine  OcrEngine    `json:"ocrEngine"`
-	Prompt     Prompt       `json:"prompt"`
-	PromptId   uint         `json:"promptId"`
+	Name        string       `gorm:"not null; uniqueIndex" json:"name"`
+	Description string       `json:"description"`
+	AiType      AiClientType `json:"type"`
+	Url         string       `json:"url"`
+	Key         string       `json:"key"`
+	Model       string       `json:"model"`
+	NumWorkers  int          `json:"numWorkers"`
+	OcrEngine   OcrEngine    `json:"ocrEngine"`
+	Prompt      Prompt       `json:"prompt"`
+	PromptId    uint         `json:"promptId"`
 }
 
 func (ReceiptProcessingSettings *ReceiptProcessingSettings) LoadDataFromRequest(w http.ResponseWriter, r *http.Request) error {

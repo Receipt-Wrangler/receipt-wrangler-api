71cf232fc80956c1f0d0c65d536d44b30e7473d2
Noah <navi0001@protonmail.com>
2024-05-26 11:21:11 -0400

    rename test files use discover command to run tests

--- imap-client/test_utils.py
+++ imap-client/test_utils.py
@@ -1,6 +1,4 @@
-import logging
 import unittest
-from io import StringIO
 
 from utils import valid_from_email, valid_subject
 
@@ -28,18 +26,6 @@ class TestEmailUtils(unittest.TestCase):
     def test_valid_subject_no_match_found(self):
         subject_line_regexes = [{"regex": r"^Hello"}]
         self.assertFalse(valid_subject("Test subject", subject_line_regexes))
-
-    def test_valid_subject_logging(self):
-        log_stream = StringIO()
-        logging.basicConfig(stream=log_stream, level=logging.INFO)
-
-        subject_line_regexes = [{"regex": r"^Test"}]
-        valid_subject("Test subject", subject_line_regexes)
-
-        log_contents = log_stream.getvalue()
-        self.assertIn("Found match:", log_contents)
-        self.assertIn("on email subject: 'Test subject'", log_contents)
-        self.assertIn("with regex: '^Test'", log_contents)
 
 
 if __name__ == "__main__":

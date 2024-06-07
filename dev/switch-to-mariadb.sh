198f7e41dfa7d1b7f9e7ff4bf8de30b921758792
Noah <navi0001@protonmail.com>
2024-06-07 08:14:21 -0400

    update dev scripts

--- dev/switch-to-mariadb.sh
+++ dev/switch-to-mariadb.sh
@@ -3,3 +3,5 @@ export DB_USER=wrangler
 export DB_NAME=wrangler
 export DB_ENGINE=mariadb
 export DB_PASSWORD=123456
+export ENCRYPTION_KEY=test
+export SECRET_KEY=test
--- dev/switch-to-sqlite.sh
+++ dev/switch-to-sqlite.sh
@@ -1,2 +1,4 @@
-export DB_FILENAME=wrangler.sqlite
+export DB_FILENAME=wrangler.db
 export DB_ENGINE=sqlite
+export ENCRYPTION_KEY=test
+export SECRET_KEY=test

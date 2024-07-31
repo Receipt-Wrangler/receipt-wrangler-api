cec8016c575a6fab85d3cc34c72d278d0a985cf7
Noah <navi0001@protonmail.com>
2024-07-31 08:41:46 -0400

    remove ollama vision response

--- internal/structs/ollama.go
+++ internal/structs/ollama.go
@@ -18,18 +18,3 @@ type OllamaTextResponse struct {
 	EvalDuration       int64  `json:"eval_duration"`
 	Error              string `json:"error"`
 }
-
-type OllamaVisionResponse struct {
-	Model              string    `json:"model"`
-	CreatedAt          time.Time `json:"created_at"`
-	Response           string    `json:"response"`
-	Done               bool      `json:"done"`
-	Context            []int     `json:"context"`
-	TotalDuration      int64     `json:"total_duration"`
-	LoadDuration       int       `json:"load_duration"`
-	PromptEvalCount    int       `json:"prompt_eval_count"`
-	PromptEvalDuration int64     `json:"prompt_eval_duration"`
-	EvalCount          int       `json:"eval_count"`
-	EvalDuration       int       `json:"eval_duration"`
-	Error              string    `json:"error"`
-}

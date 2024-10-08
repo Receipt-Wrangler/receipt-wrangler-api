4bd040a69fbe755768348ef0b0b9c73694f5ce37
Noah <navi0001@protonmail.com>
2024-10-08 08:17:08 -0400

    add to model

---
+++ internal/constants/locales.go
@@ -0,0 +1,29 @@
+package constants
+
+var SupportedLocales = [25]string{
+	"en-US", // United States Dollar
+	"en-GB", // British Pound Sterling
+	"ja-JP", // Japanese Yen
+	"de-DE", // Euro (Germany)
+	"fr-FR", // Euro (France)
+	"it-IT", // Euro (Italy)
+	"es-ES", // Euro (Spain)
+	"zh-CN", // Chinese Yuan
+	"ru-RU", // Russian Ruble
+	"ar-SA", // Saudi Riyal
+	"hi-IN", // Indian Rupee
+	"pt-BR", // Brazilian Real
+	"ko-KR", // South Korean Won
+	"tr-TR", // Turkish Lira
+	"nl-NL", // Euro (Netherlands)
+	"pl-PL", // Polish Złoty
+	"sv-SE", // Swedish Krona
+	"da-DK", // Danish Krone
+	"fi-FI", // Euro (Finland)
+	"no-NO", // Norwegian Krone
+	"he-IL", // Israeli New Shekel
+	"th-TH", // Thai Baht
+	"vi-VN", // Vietnamese Đồng
+	"id-ID", // Indonesian Rupiah
+	"ms-MY", // Malaysian Ringgit
+}
--- internal/models/system_settings.go
+++ internal/models/system_settings.go
@@ -4,9 +4,12 @@ type SystemSettings struct {
 	BaseModel
 	EnableLocalSignUp                   bool                      `json:"enableLocalSignUp" gorm:"default:false"`
 	DebugOcr                            bool                      `json:"debugOcr" gorm:"default:false"`
-	CurrencyDisplay                     string                    `json:"currencyDisplay" gorm:"default:$"`
 	NumWorkers                          int                       `json:"numWorkers"`
 	EmailPollingInterval                int                       `json:"emailPollingInterval" gorm:"default:1800"`
+	CurrencyLocale                      string                    `json:"currencyLocale" gorm:"default:en-US"`
+	CurrencyCode                        string                    `json:"currencyCode" gorm:"default:USD"`
+	ShowCurrencySymbol                  bool                      `json:"showCurrencySymbol"`
+	CurrencyDisplay                     string                    `json:"currencyDisplay" gorm:"default:$"`
 	ReceiptProcessingSettings           ReceiptProcessingSettings `json:"-"`
 	ReceiptProcessingSettingsId         *uint                     `json:"receiptProcessingSettingsId"`
 	FallbackReceiptProcessingSettings   ReceiptProcessingSettings `json:"-"`

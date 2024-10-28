580cf53aef9c96e0f08e89c92060664b976aee19
Noah <navi0001@protonmail.com>
2024-10-28 09:14:44 -0400

    re implement env variables

--- internal/repositories/about.go
+++ internal/repositories/about.go
@@ -2,6 +2,7 @@ package repositories
 
 import (
 	"gorm.io/gorm"
+	"os"
 	"receipt-wrangler/api/internal/structs"
 )
 
@@ -18,8 +19,16 @@ func NewAboutRepository(tx *gorm.DB) AboutRepository {
 }
 
 func (repository AboutRepository) GetAboutData() (structs.About, error) {
-	envVersion := "latest"
-	envBuildDate := ""
+	envVersion := os.Getenv("VERSION")
+	envBuildDate := os.Getenv("BUILD_DATE")
+
+	if len(envVersion) == 0 {
+		envVersion = "unknown"
+	}
+
+	if len(envBuildDate) == 0 {
+		envBuildDate = "unknown"
+	}
 
 	about := structs.About{
 		Version:   envVersion,

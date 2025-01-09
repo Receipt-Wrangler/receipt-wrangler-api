8f032362bbe193fcbbf3ba947db111a1bf468eba
Noah231515 <noah231515@gmail.com>
2025-01-09 19:52:16 +0000

    split queue names into separate file

---
+++ internal/wranglerasynq/queue_names.go
@@ -0,0 +1,10 @@
+package wranglerasynq
+
+type QueueName string
+
+const (
+	QuickScanQueue                QueueName = "quick_scan"
+	EmailPollingQueue             QueueName = "email_polling"
+	EmailReceiptProcessingQueue   QueueName = "email_receipt_processing"
+	EmailReceiptImageCleanupQueue QueueName = "email_receipt_image_cleanup"
+)
--- internal/wranglerasynq/task_enqueue.go
+++ internal/wranglerasynq/task_enqueue.go
@@ -5,15 +5,6 @@ import (
 	"receipt-wrangler/api/internal/repositories"
 )
 
-type QueueName string
-
-const (
-	QuickScanQueue                QueueName = "quick_scan"
-	EmailPollingQueue             QueueName = "email_polling"
-	EmailReceiptProcessingQueue   QueueName = "email_receipt_processing"
-	EmailReceiptImageCleanupQueue QueueName = "email_receipt_image_cleanup"
-)
-
 func EnqueueTask(task *asynq.Task, queue QueueName) (*asynq.TaskInfo, error) {
 	client := repositories.GetAsynqClient()
 	return client.Enqueue(task, asynq.MaxRetry(3), asynq.Queue(string(queue)))

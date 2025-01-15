c3f9da341274ffbae4f5862fbed9922a0c17a407
Noah231515 <noah231515@gmail.com>
2025-01-15 13:55:38 +0000

    add activity struct

---
+++ internal/structs/activity.go
@@ -0,0 +1,17 @@
+package structs
+
+import (
+	"receipt-wrangler/api/internal/models"
+	"time"
+)
+
+type Activity struct {
+	Id                uint                    `json:"id"`
+	Type              models.SystemTaskType   `json:"type"`
+	Status            models.SystemTaskStatus `json:"status"`
+	StartedAt         time.Time               `json:"startedAt"`
+	EndedAt           *time.Time              `json:"endedAt"`
+	ResultDescription string                  `json:"resultDescription"`
+	RanByUserId       *uint                   `json:"ranByUserId"`
+	ChildSystemTasks  []*models.SystemTask    `json:"childSystemTasks"`
+}

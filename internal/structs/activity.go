6ee8fe532e21625047da6a03a3b7e3a1582a7213
Noah231515 <noah231515@gmail.com>
2025-01-18 13:33:47 +0000

    remove field

--- internal/structs/activity.go
+++ internal/structs/activity.go
@@ -6,12 +6,11 @@ import (
 )
 
 type Activity struct {
-	Id                uint                    `json:"id"`
-	Type              models.SystemTaskType   `json:"type"`
-	Status            models.SystemTaskStatus `json:"status"`
-	StartedAt         time.Time               `json:"startedAt"`
-	EndedAt           *time.Time              `json:"endedAt"`
-	ResultDescription string                  `json:"resultDescription"`
-	RanByUserId       *uint                   `json:"ranByUserId"`
-	CanBeRestarted    bool                    `json:"canBeRestarted"`
+	Id             uint                    `json:"id"`
+	Type           models.SystemTaskType   `json:"type"`
+	Status         models.SystemTaskStatus `json:"status"`
+	StartedAt      time.Time               `json:"startedAt"`
+	EndedAt        *time.Time              `json:"endedAt"`
+	RanByUserId    *uint                   `json:"ranByUserId"`
+	CanBeRestarted bool                    `json:"canBeRestarted"`
 }

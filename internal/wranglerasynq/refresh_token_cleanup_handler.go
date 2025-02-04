ebb03483acfd3aee7c1dc1ff970b1a5468b5dc14
Noah231515 <noah231515@gmail.com>
2025-02-04 15:46:30 +0000

    remove unhashed tokens

--- internal/wranglerasynq/refresh_token_cleanup_handler.go
+++ internal/wranglerasynq/refresh_token_cleanup_handler.go
@@ -9,10 +9,10 @@ import (
 )
 
 func HandleRefreshTokenCleanupTask(context context.Context, task *asynq.Task) error {
-
+	// NOTE: Length check can be removed in a while. Length check was added to remove refresh tokens that were not hashed
 	db := repositories.GetDB()
 	return db.Model(&models.RefreshToken{}).
-		Where("expires_at < ? OR is_used = ?", time.Now(), true).
+		Where("expires_at < ? OR is_used = ? OR length(token) > 64", time.Now(), true).
 		Delete(&models.RefreshToken{}).
 		Error
 }

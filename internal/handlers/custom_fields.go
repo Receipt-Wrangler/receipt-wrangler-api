3ce5234559742c37e44c8747735f16b9dda918f9
noah231515 <noah231515@gmail.com>
2025-03-04 13:35:53 +0000

    add get paged custom fields endpoint

--- internal/handlers/custom_fields.go
+++ internal/handlers/custom_fields.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"github.com/go-chi/chi/v5"
 	"net/http"
 	"receipt-wrangler/api/internal/commands"
 	"receipt-wrangler/api/internal/constants"
@@ -12,38 +11,35 @@ import (
 )
 
 func GetPagedCustomFields(w http.ResponseWriter, r *http.Request) {
-	groupId := chi.URLParam(r, "groupId")
 	handler := structs.Handler{
 		ErrorMessage: "Error getting receipts",
 		Writer:       w,
 		Request:      r,
-		GroupId:      groupId,
-		GroupRole:    models.VIEWER,
+		UserRole:     models.USER,
 		ResponseType: constants.ApplicationJson,
 		HandlerFunction: func(w http.ResponseWriter, r *http.Request) (int, error) {
-			pagedRequest := commands.ReceiptPagedRequestCommand{}
-			err := pagedRequest.LoadDataFromRequest(w, r)
-			if err != nil {
-				return http.StatusInternalServerError, err
-			}
-
 			pagedData := structs.PagedData{}
-			token := structs.GetJWT(r)
-
-			receiptRepository := repositories.NewReceiptRepository(nil)
-			receipts, count, err := receiptRepository.GetPagedReceiptsByGroupId(
-				token.UserId,
-				groupId,
-				pagedRequest,
-				nil,
-			)
+			pagedRequestCommand := commands.PagedRequestCommand{}
+			err := pagedRequestCommand.LoadDataFromRequest(w, r)
 			if err != nil {
 				return http.StatusInternalServerError, err
 			}
 
-			anyData := make([]any, len(receipts))
-			for i := 0; i < len(receipts); i++ {
-				anyData[i] = receipts[i]
+			vErrs := pagedRequestCommand.Validate()
+			if len(vErrs.Errors) > 0 {
+				structs.WriteValidatorErrorResponse(w, vErrs, http.StatusBadRequest)
+				return 0, nil
+			}
+
+			customFieldsRepository := repositories.NewCustomFieldRepository(nil)
+			customFields, count, err := customFieldsRepository.GetPagedCustomFields(pagedRequestCommand)
+			if err != nil {
+				return http.StatusInternalServerError, err
+			}
+
+			anyData := make([]any, len(customFields))
+			for i := 0; i < len(customFields); i++ {
+				anyData[i] = customFields[i]
 			}
 
 			pagedData.Data = anyData
--- internal/repositories/custom_fields.go
+++ internal/repositories/custom_fields.go
@@ -2,6 +2,8 @@ package repositories
 
 import (
 	"gorm.io/gorm"
+	"receipt-wrangler/api/internal/commands"
+	"receipt-wrangler/api/internal/models"
 )
 
 type CustomFieldRepository struct {
@@ -14,4 +16,25 @@ func NewCustomFieldRepository(tx *gorm.DB) CustomFieldRepository {
 		TX: tx,
 	}}
 	return repository
+}
+
+func (repository CustomFieldRepository) GetPagedCustomFields(pagedRequestCommand commands.PagedRequestCommand) ([]models.CustomField, int64, error) {
+	db := repository.GetDB()
+	var customFields []models.CustomField
+
+	query := repository.Sort(db, pagedRequestCommand.OrderBy, pagedRequestCommand.SortDirection)
+	query = query.Scopes(repository.Paginate(pagedRequestCommand.Page, pagedRequestCommand.PageSize))
+
+	err := query.Scan(&customFields).Error
+	if err != nil {
+		return nil, 0, err
+	}
+
+	var count int64
+	err = db.Model(&models.CustomField{}).Count(&count).Error
+	if err != nil {
+		return nil, 0, err
+	}
+
+	return customFields, count, nil
 }

57dc5ddb02d76fd2416033419078f639e2e76c42
noah231515 <noah231515@gmail.com>
2025-03-11 12:30:28 +0000

    set up commands

---
+++ internal/commands/upsert_custom_field_command.go
@@ -0,0 +1,47 @@
+package commands
+
+import (
+	"encoding/json"
+	"net/http"
+	"receipt-wrangler/api/internal/models"
+	"receipt-wrangler/api/internal/structs"
+	"receipt-wrangler/api/internal/utils"
+)
+
+type UpsertCustomFieldCommand struct {
+	Id          *uint                            `json:"id"`
+	Name        string                           `json:"name"`
+	Type        models.CustomFieldType           `json:"type"`
+	Description string                           `json:"description"`
+	Options     []UpsertCustomFieldOptionCommand `json:"options"`
+}
+
+func (command *UpsertCustomFieldCommand) LoadDataFromRequest(w http.ResponseWriter, r *http.Request) error {
+	bytes, err := utils.GetBodyData(w, r)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(bytes, &command)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (command *UpsertCustomFieldCommand) Validate() structs.ValidatorError {
+	errors := make(map[string]string)
+	vErr := structs.ValidatorError{}
+
+	if len(command.Name) == 0 {
+		errors["name"] = "Name is required"
+	}
+
+	if len(command.Type) == 0 {
+		errors["type"] = "Type is required"
+	}
+
+	vErr.Errors = errors
+	return vErr
+}

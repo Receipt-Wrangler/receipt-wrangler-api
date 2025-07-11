03a5f579fb8257cbf270ac3691d16cd8b9132f17
noah231515 <noah231515@gmail.com>
2025-07-11 15:19:06 +0000

    remove null

--- internal/models/group_receipt_settings.go
+++ internal/models/group_receipt_settings.go
@@ -3,10 +3,10 @@ package models
 type GroupReceiptSettings struct {
 	BaseModel
 	GroupId               uint `gorm:"not null;unique" json:"groupId"`
-	HideImages            bool `gorm:"not null" json:"hideImages"`
-	HideReceiptCategories bool `gorm:"not null" json:"hideReceiptCategories"`
-	HideReceiptTags       bool `gorm:"not null" json:"hideReceiptTags"`
-	HideShareCategories   bool `gorm:"not null" json:"hideShareCategories"`
-	HideShareTags         bool `gorm:"not null" json:"hideShareTags"`
-	HideComments          bool `gorm:"not null" json:"hideComments"`
+	HideImages            bool `json:"hideImages"`
+	HideReceiptCategories bool `json:"hideReceiptCategories"`
+	HideReceiptTags       bool `json:"hideReceiptTags"`
+	HideShareCategories   bool `json:"hideShareCategories"`
+	HideShareTags         bool `json:"hideShareTags"`
+	HideComments          bool `json:"hideComments"`
 }

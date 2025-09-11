c5b5133421f374302aec894a7f6aa76650eec44e
noah231515 <noah231515@gmail.com>
2025-09-11 14:29:06 +0000

    stub out flow

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -3,17 +3,32 @@ package middleware
 import (
 	"net/http"
 	"receipt-wrangler/api/internal/constants"
+	"receipt-wrangler/api/internal/logging"
 	"receipt-wrangler/api/internal/utils"
 	"strings"
 )
 
 func UnifiedAuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		const unauthorized = "Unauthorized"
 
-		if ApiKeyExists(*r) {
-			// validate api key
-		} else if JwtExists(*r) {
-			// validate jwt
+		apiKey := getApiKey(*r)
+		jwt := getJwt(*r)
+
+		if len(apiKey) != 0 {
+			err := validateApiKey(*r)
+			if err != nil {
+				logging.LogStd(logging.LOG_LEVEL_ERROR, err.Error())
+				utils.WriteCustomErrorResponse(w, unauthorized, http.StatusForbidden)
+				return
+			}
+		} else if len(jwt) != 0 {
+			err := validateJwt(*r)
+			if err != nil {
+				logging.LogStd(logging.LOG_LEVEL_ERROR, err.Error())
+				utils.WriteCustomErrorResponse(w, unauthorized, http.StatusForbidden)
+				return
+			}
 		}
 
 		utils.WriteCustomErrorResponse(w, "Unauthorized", http.StatusForbidden)
@@ -21,20 +36,43 @@ func UnifiedAuthMiddleware(next http.Handler) http.Handler {
 	})
 }
 
-func ApiKeyExists(r http.Request) bool {
+func validateJwt(r http.Request) error {
+	return nil
+}
+
+func validateApiKey(r http.Request) error {
+	return nil
+}
+
+func getApiKey(r http.Request) string {
 	authHeader := r.Header.Get("Authorization")
 	parts := strings.Split(authHeader, ".")
 
 	if len(parts) != 4 {
-		return false
+		return ""
 	}
 
-	return parts[0] == constants.V1Prefix
+	if parts[0] == constants.V1Prefix {
+		return authHeader
+	}
+
+	return ""
 }
 
-func JwtExists(r http.Request) bool {
-	bearerExists := strings.Contains(r.Header.Get("Authorization"), "Bearer")
-	authCookieExists, err := r.Cookie(constants.JwtKey)
+func getJwt(r http.Request) string {
+	authCookie, err := r.Cookie(constants.JwtKey)
+	if err != nil {
+		logging.LogStd(logging.LOG_LEVEL_ERROR, err.Error())
+	}
 
-	return bearerExists || (authCookieExists != nil && err == nil)
+	if authCookie != nil && len(authCookie.Value) != 0 {
+		return authCookie.Value
+	}
+
+	authHeader := r.Header.Get("Authorization")
+	if len(authHeader) == 0 {
+		return ""
+	}
+
+	return authHeader
 }

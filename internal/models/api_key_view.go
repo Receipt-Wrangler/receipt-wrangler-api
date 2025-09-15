1caede1fba9fc57d973f97ede8088200b8d41028
noah231515 <noah231515@gmail.com>
2025-09-15 14:29:51 +0000

    get api keys endpoint

---
+++ internal/models/api_key_view.go
@@ -0,0 +1,17 @@
+package models
+
+import "time"
+
+type ApiKeyView struct {
+	ID              string     `json:"id"`
+	CreatedAt       time.Time  `json:"createdAt"`
+	UpdatedAt       time.Time  `json:"updatedAt"`
+	CreatedBy       *uint      `json:"createdBy"`
+	CreatedByString string     `json:"createdByString"`
+	Name            string     `json:"name"`
+	Description     string     `json:"description"`
+	UserID          *uint      `json:"userId"`
+	Scope           string     `json:"scope"`
+	LastUsedAt      *time.Time `json:"lastUsedAt"`
+	RevokedAt       *time.Time `json:"revokedAt"`
+}
